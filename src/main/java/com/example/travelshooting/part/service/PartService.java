3a30e02e6f2a12724252c314df1a9744db71d340
jiyeon0926 <yeoni9094@naver.com>
2025-01-09 04:30:17 +0900

    feat: findPartByProductId() 메서드 추가
    productId로 part 찾기

--- src/main/java/com/example/travelshooting/part/service/PartService.java
+++ src/main/java/com/example/travelshooting/part/service/PartService.java
@@ -15,4 +15,14 @@ public class PartService {
         return partRepository.findById(partId)
                 .orElseThrow(() -> new IllegalArgumentException("아이디 " + partId + "에 해당하는 레저/티켓 일정을 찾을 수 없습니다."));
     }
+
+    public Part findPartByProductId(Long productId) {
+        Part part = partRepository.findPartByProductId(productId);
+
+        if (part == null) {
+            throw new IllegalArgumentException("아이디 " + part.getId() + "에 해당하는 레저/티켓 일정을 찾을 수 없습니다.");
+        }
+
+        return part;
+    }
 }
