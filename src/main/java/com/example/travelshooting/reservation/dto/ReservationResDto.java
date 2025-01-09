a8ae1aa2b4155e904bc2611e2979fa1656345717
jiyeon0926 <yeoni9094@naver.com>
2025-01-09 17:03:03 +0900

    feat: 업체 기준 예약 전체 조회

--- src/main/java/com/example/travelshooting/reservation/dto/ReservationResDto.java
+++ src/main/java/com/example/travelshooting/reservation/dto/ReservationResDto.java
@@ -3,23 +3,35 @@ package com.example.travelshooting.reservation.dto;
 import com.example.travelshooting.common.BaseDtoDataType;
 import com.example.travelshooting.enums.ReservationStatus;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
 public class ReservationResDto implements BaseDtoDataType {
 
-    private final Long id;
-    private final Long userId;
-    private final Long productId;
-    private final Long partId;
-    private final LocalDate reservationDate;
-    private final int number;
-    private final int totalPrice;
-    private final ReservationStatus status;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime updatedAt;
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private Long partId;
+    private LocalDate reservationDate;
+    private int number;
+    private int totalPrice;
+    private ReservationStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public ReservationResDto(Long id, Long userId, Long productId, Long partId, LocalDate reservationDate,
+                             int number, int totalPrice, ReservationStatus status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.partId = partId;
+        this.reservationDate = reservationDate;
+        this.number = number;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/example/travelshooting/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/travelshooting/reservation/repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.example.travelshooting.reservation.repository;
 
 import com.example.travelshooting.reservation.Reservation;
+import com.example.travelshooting.reservation.dto.ReservationResDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findAllByUserIdAndProductId(Long userId, Long productId);
 
     Reservation findByUserIdAndProductIdAndId(Long userId, Long productId, Long reservationId);
+
+    @Query("SELECT new com.example.travelshooting.reservation.dto.ReservationResDto(r.id, r.user.id, r.product.id, r.part.id, r.reservationDate, r.number, r.totalPrice, r.status, r.createdAt, r.updatedAt) " +
+            "FROM Reservation r " +
+            "JOIN r.product p " +
+            "JOIN p.company c " +
+            "WHERE p.id = :productId AND c.user.id = :userId")
+    List<ReservationResDto> findByProductIdAndUserId(@Param("productId") Long productId, @Param("userId") Long userId);
 }
