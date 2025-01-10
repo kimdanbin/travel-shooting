1c8bc00097b3edfff6ad66aba5c2382e005e6b1d
kimdanbin <rlaeksqls@naver.com>
2025-01-10 17:57:56 +0900

    fix: 경로수정

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user (email, password, role, is_deleted, created_at, updated_at)
-VALUES ('asdf', 'asdf', 'ADMIN', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO restaurant (place_name, address_name, latitude, longitude, phone, road_address_name)
-VALUES ('asdf', 'asdf', 'asdf', 'asdf', '01012341234', 'asdf')
