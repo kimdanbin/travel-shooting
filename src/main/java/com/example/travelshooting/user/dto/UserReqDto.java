29dd2eba76f16c6c421ea528e4ad97eb0202a563
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2025-01-10 15:45:20 +0900

    feat: 관리자, 파트너 회원가입 기능 구현

---
+++ src/main/java/com/example/travelshooting/user/dto/UserReqDto.java
@@ -0,0 +1,25 @@
+package com.example.travelshooting.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UserReqDto {
+
+  @Email(message = "올바른 email 형식이 아닙니다.")
+  @Size(max = 200)
+  private String email;
+
+  @Pattern(
+      regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+      message = "비밀번호는 최소 8자 이상, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+  )
+  private String password;
+
+  @NotBlank(message = "name은 빈 값이 허용되지 않습니다.")
+  @Size(max = 10)
+  private String name;
+}
---
+++ src/main/java/com/example/travelshooting/user/service/AdminService.java
@@ -0,0 +1,43 @@
+package com.example.travelshooting.user.service;
+
+import com.example.travelshooting.enums.UserRole;
+import com.example.travelshooting.user.User;
+import com.example.travelshooting.user.dto.UserResDto;
+import com.example.travelshooting.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Transactional
+  public UserResDto adminSignup(String email, String password, String name) {
+    Optional<User> optionalUser = userRepository.findByEmail(email);
+
+    if (optionalUser.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
+    }
+
+    String encodedPassword = passwordEncoder.encode(password);
+
+    User user = new User(email, encodedPassword, name, UserRole.ADMIN);
+    User savedUser = userRepository.save(user);
+
+    return new UserResDto(
+        savedUser.getId(),
+        savedUser.getEmail(),
+        savedUser.getName(),
+        savedUser.getRole()
+    );
+  }
+}
