5d2621b8cd41756a4d7cd18eab49fcf7f43b28c4
jiyeon0926 <yeoni9094@naver.com>
2025-01-15 23:30:22 +0900

    refactor: 장소 검색 리팩토링
    - 장소 검색 시, "장소 검색 완료" 메시지도 포함 시킴

--- src/main/java/com/example/travelshooting/local/controller/LocalController.java
+++ src/main/java/com/example/travelshooting/local/controller/LocalController.java
@@ -1,14 +1,18 @@
 package com.example.travelshooting.local.controller;
 
+import com.example.travelshooting.common.CommonListResDto;
 import com.example.travelshooting.local.dto.LocalResDto;
 import com.example.travelshooting.local.service.LocalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +21,10 @@ public class LocalController {
     private final LocalService localService;
 
     @GetMapping("/locals")
-    public ResponseEntity<Page<LocalResDto>> searchPlaces(@RequestParam String keyword, Pageable pageable) {
-        return ResponseEntity.ok().body(localService.searchPlaces(keyword, pageable));
+    public ResponseEntity<CommonListResDto<LocalResDto>> searchPlaces(@RequestParam String keyword, Pageable pageable) {
+        Page<LocalResDto> local = localService.searchPlaces(keyword, pageable);
+        List<LocalResDto> content = local.getContent();
+
+        return new ResponseEntity<>(new CommonListResDto<>("장소 검색 완료", content), HttpStatus.OK);
     }
 }
