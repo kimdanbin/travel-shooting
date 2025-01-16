cf35e9fa14f3408126dcd404cd89e99e2494c7ec
이아름 <xox_lol@naver.com>
2025-01-16 10:28:46 +0900

    refactor: toDto 제거

--- src/main/java/com/example/travelshooting/product/dto/ProductDetailResDto.java
+++ src/main/java/com/example/travelshooting/product/dto/ProductDetailResDto.java
@@ -2,13 +2,11 @@ package com.example.travelshooting.product.dto;
 
 import com.example.travelshooting.common.BaseDtoDataType;
 import com.example.travelshooting.part.dto.PartResDto;
-import com.example.travelshooting.product.entity.Product;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @RequiredArgsConstructor
@@ -25,21 +23,4 @@ public class ProductDetailResDto implements BaseDtoDataType {
     private final LocalDateTime updatedAt;
     private final List<PartResDto> parts;
 
-    public static ProductDetailResDto toDto(Product product) {
-        List<PartResDto> parts = product.getParts().stream()
-                .map(part -> new PartResDto(part.getId(), part.getOpenAt(), part.getCloseAt(), part.getNumber()))
-                .collect(Collectors.toList());
-        return new ProductDetailResDto(
-                product.getId(),
-                product.getCompany().getId(),
-                product.getCompany().getName(),
-                product.getCompany().getDescription(),
-                product.getPrice(),
-                product.getAddress(),
-                product.getQuantity(),
-                product.getCreatedAt(),
-                product.getUpdatedAt(),
-                parts
-        );
-    }
 }
