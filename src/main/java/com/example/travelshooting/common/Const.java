d40ad2c7b52d4aa923ae627bee500060258ed083
jiyeon0926 <yeoni9094@naver.com>
2025-01-17 08:47:48 +0900

    refactor: 환경변수 및 상수로 관리

--- src/main/java/com/example/travelshooting/common/Const.java
+++ src/main/java/com/example/travelshooting/common/Const.java
@@ -3,6 +3,9 @@ package com.example.travelshooting.common;
 public class Const {
 
     public static final String GG_NAME = "경기도";
-    public static final int CONNECT_TIMEOUT = 5000;
-    public static final int READ_TIMEOUT = 5000;
+    public static final Integer CONNECT_TIMEOUT = 5000;
+    public static final Integer READ_TIMEOUT = 5000;
+    public static final String KAKAO_PAY_TEST_CID = "TC0ONETIME";
+    public static final String KAKAO_PAY_HEADER = "SECRET_KEY";
+    public static final String KAKAO_MAP_HEADER = "KakaoAK";
 }
