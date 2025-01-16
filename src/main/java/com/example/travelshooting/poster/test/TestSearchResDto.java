7bf2721abb588d2eca0cbd13e5f6471ba5d5c165
jiyeon0926 <yeoni9094@naver.com>
2025-01-16 17:46:38 +0900

    feat: 좋아요순 포스트 검색 기능
    - 임시 테스트 파일
    - querydsl 적용해 검색 테스트

---
+++ src/main/java/com/example/travelshooting/poster/test/TestSearchResDto.java
@@ -0,0 +1,29 @@
+package com.example.travelshooting.poster.test;
+
+import com.example.travelshooting.common.BaseDtoDataType;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class TestSearchResDto implements BaseDtoDataType {
+
+    private final Long id;
+    private final String title;
+    private final int expenses;
+    private final LocalDateTime travelStartAt;
+    private final LocalDateTime travelEndAt;
+    private final Integer likeCount;
+
+    @QueryProjection
+    public TestSearchResDto(Long id, String title, int expenses, LocalDateTime travelStartAt, LocalDateTime travelEndAt, Integer likeCount) {
+        this.id = id;
+        this.title = title;
+        this.expenses = expenses;
+        this.travelStartAt = travelStartAt;
+        this.travelEndAt = travelEndAt;
+        this.likeCount = likeCount;
+    }
+
+}
