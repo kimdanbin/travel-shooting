32e7e1d557fa078cc18834457999de42f1f882c4
jiyeon0926 <yeoni9094@naver.com>
2025-01-20 10:09:37 +0900

    fix: part entity
    product와 fetch

--- src/main/java/com/example/travelshooting/part/entity/Part.java
+++ src/main/java/com/example/travelshooting/part/entity/Part.java
@@ -25,7 +25,7 @@ public class Part extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "leisure_product_id", nullable = false)
     private Product product;
 
