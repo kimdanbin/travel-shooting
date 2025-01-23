d78ef481689d504dc337980e44178e30a4efdd56
jiyeon0926 <yeoni9094@naver.com>
2025-01-23 23:33:37 +0900

    fix: jwt 필터 수정

--- src/main/java/com/example/travelshooting/config/filter/JwtAuthFilter.java
+++ src/main/java/com/example/travelshooting/config/filter/JwtAuthFilter.java
@@ -23,70 +23,76 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-  private final JwtProvider jwtProvider;
-  private final UserDetailsService userDetailsService;
+    private final JwtProvider jwtProvider;
+    private final UserDetailsService userDetailsService;
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                  FilterChain filterChain) throws ServletException, IOException {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-    authenticate(request);
-    filterChain.doFilter(request, response);
-  }
-
-  /**
-   * request를 이용해 인증을 처리한다.
-   *
-   * @param request {@link HttpServletRequest}
-   */
-  private void authenticate(HttpServletRequest request) {
-    // 토큰 검증.
-    String token = getTokenFromRequest(request);
-
-    if (!jwtProvider.validToken(token)) {
-      return;
+        authenticate(request);
+        filterChain.doFilter(request, response);
     }
 
-    String username = jwtProvider.getUsername(token);
-    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    /**
+     * request를 이용해 인증을 처리한다.
+     *
+     * @param request {@link HttpServletRequest}
+     */
+    private void authenticate(HttpServletRequest request) {
+        // 토큰 검증.
+        String token = getTokenFromRequest(request);
 
-    setAuthentication(request, userDetails);
-  }
+        if (token != null) {
+            if (!jwtProvider.validToken(token)) {
+                return;
+            }
 
-  /**
-   * request의 Authorization 헤더에서 토큰 값을 추출.
-   *
-   * @param request {@link HttpServletRequest}
-   * @return 토큰 값 (찾지 못한 경우 {@code null})
-   */
-  private String getTokenFromRequest(HttpServletRequest request) {
-    final String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
-    final String headerPrefix = AuthenticationScheme.generateType(AuthenticationScheme.BEARER);
+            // 토큰으로부텨 username을 추출.
+            String username = jwtProvider.getUsername(token);
 
-    boolean tokenFound = StringUtils.hasText(bearerToken) && bearerToken.startsWith(headerPrefix);
+            // username에 해당되는 사용자를 찾는다.
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-    if (tokenFound) {
-      return bearerToken.substring(headerPrefix.length());
+            // SecurityContext에 인증 객체 저장.
+            setAuthentication(request, userDetails);
+        }
     }
 
-    return null;
-  }
+    /**
+     * request의 Authorization 헤더에서 토큰 값을 추출.
+     *
+     * @param request {@link HttpServletRequest}
+     * @return 토큰 값 (찾지 못한 경우 {@code null})
+     */
+    private String getTokenFromRequest(HttpServletRequest request) {
+        final String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String headerPrefix = AuthenticationScheme.generateType(AuthenticationScheme.BEARER);
 
-  /**
-   * {@code SecurityContext}에 인증 객체를 저장한다.
-   *
-   * @param request     {@link HttpServletRequest}
-   * @param userDetails 찾아온 사용자 정보
-   */
-  private void setAuthentication(HttpServletRequest request, UserDetails userDetails) {
-    // 찾아온 사용자 정보로 인증 객체를 생성.
-    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
-    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        boolean tokenFound = StringUtils.hasText(bearerToken) && bearerToken.startsWith(headerPrefix);
 
-    // SecurityContext에 인증 객체 저장.
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-  }
+        if (tokenFound) {
+            return bearerToken.substring(headerPrefix.length());
+        }
+
+        return null;
+    }
+
+    /**
+     * {@code SecurityContext}에 인증 객체를 저장한다.
+     *
+     * @param request     {@link HttpServletRequest}
+     * @param userDetails 찾아온 사용자 정보
+     */
+    private void setAuthentication(HttpServletRequest request, UserDetails userDetails) {
+        // 찾아온 사용자 정보로 인증 객체를 생성.
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // SecurityContext에 인증 객체 저장.
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 }
