38d453cf0d20737a8ba5122fadfb545179bf8d06
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2025-01-26 18:25:57 +0900

    feat: Redis setting

---
+++ src/main/java/com/example/travelshooting/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.example.travelshooting.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
+  @Value("${spring.data.redis.password:}")
+  private String redisPassword;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+    if (!redisPassword.isEmpty()) {
+      config.setPassword(redisPassword); // 비밀번호 설정
+    }
+    return new LettuceConnectionFactory(config);
+  }
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory2() {
+    return new LettuceConnectionFactory(redisHost,redisPort);
+  }
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    return redisTemplate;
+  }
+  // Value 값을 Object로 저장시 직렬화 수정한 임시 코드
+//  @Bean
+//  public RedisTemplate<String, Object> redisTemplate() {
+//    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//    redisTemplate.setConnectionFactory(redisConnectionFactory());
+//
+//    // 직렬화 설정
+//    redisTemplate.setKeySerializer(new StringRedisSerializer());
+//    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//
+//    return redisTemplate;
+//  }
+
+}
