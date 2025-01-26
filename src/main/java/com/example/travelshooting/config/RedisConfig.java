7490bf06b564ffbbde8b15c0404f35da3c2336bf
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2025-01-26 18:49:00 +0900

    fix: 코드리뷰 반영, 중복 코드 제거

--- src/main/java/com/example/travelshooting/config/RedisConfig.java
+++ src/main/java/com/example/travelshooting/config/RedisConfig.java
@@ -7,10 +7,12 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
   @Value("${spring.data.redis.host}")
   private String redisHost;
 
@@ -26,33 +28,33 @@ public class RedisConfig {
     if (!redisPassword.isEmpty()) {
       config.setPassword(redisPassword); // 비밀번호 설정
     }
+
     return new LettuceConnectionFactory(config);
   }
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory2() {
-    return new LettuceConnectionFactory(redisHost,redisPort);
-  }
+
   @Bean
   public RedisTemplate<String, String> redisTemplate() {
     RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
+
     return redisTemplate;
   }
-  // Value 값을 Object로 저장시 직렬화 수정한 임시 코드
-//  @Bean
-//  public RedisTemplate<String, Object> redisTemplate() {
-//    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//    redisTemplate.setConnectionFactory(redisConnectionFactory());
-//
-//    // 직렬화 설정
-//    redisTemplate.setKeySerializer(new StringRedisSerializer());
-//    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-//
-//    return redisTemplate;
-//  }
+
+  // Value 값을 Object로 저장시 직렬화
+  @Bean
+  public RedisTemplate<String, Object> redisObjectTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+    // 직렬화 설정
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+    return redisTemplate;
+  }
 
 }
