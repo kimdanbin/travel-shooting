a912c4abd476a3ac84620ced0f4355693f0f9c9d
kimdanbin <rlaeksqls@naver.com>
2025-01-27 21:02:08 +0900

    fix: docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ECR_REPOSITORY=$AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/travel-shooting
+ECR_REPOSITORY=${1}.dkr.ecr.${2}.amazonaws.com/travel-shooting
 IMAGE_TAG=latest
 
 # Pull new image
