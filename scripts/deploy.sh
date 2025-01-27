f46815e20a0804b73f3fcaae046b7640df2a802b
kimdanbin <rlaeksqls@naver.com>
2025-01-27 20:01:48 +0900

    feat: dockerfile 생성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ECR_REPOSITORY=$AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/aws-demo
+ECR_REPOSITORY=$AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/aws-travel-shooting
 IMAGE_TAG=latest
 
 # Pull new image
