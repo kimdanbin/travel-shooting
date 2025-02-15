7b79d805f66381719fabdca71a1325f58747c2f5
jiyeon0926 <yeoni9094@naver.com>
2025-02-15 17:04:45 +0900

    fix: ResDto Valid 제거
    - Valid는 요청 검증을 할 때 사용하기 때문에 응답을 반환하는 ResDto에 Valid를 사용할 필요 없음

--- src/main/java/com/example/travelshooting/comment/dto/CommentResDto.java
+++ src/main/java/com/example/travelshooting/comment/dto/CommentResDto.java
@@ -2,7 +2,6 @@ package com.example.travelshooting.comment.dto;
 
 import com.example.travelshooting.comment.entity.Comment;
 import com.example.travelshooting.common.BaseDtoDataType;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
@@ -10,12 +9,10 @@ public class CommentResDto implements BaseDtoDataType {
 
     private Long id;
 
-    @NotBlank(message = "댓글을 입력해주세요.")
     private String content;
 
     public CommentResDto(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
     }
-
 }
